832561486667512a018fa988193da533c08e7261
Nguyen Duy Minh <minhnguyenduy99@gmail.com>
2024-06-09 19:56:40 +0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        tabWidth: 4,
+        trailingComma: "all",
+        semi: false,
+        useTabs: false,
+      },
+    ], // Use Prettier with the specified rules. In this case, it's set to use single quotes and the flow parser.
+  },
+};
---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+
+@Module({
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {
+    constructor() {
+        for (let i = 0; i < 10; i++) {
+            console.log(i)
+        }
+    }
+}
